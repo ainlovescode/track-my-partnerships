3107f991800523c6a0939740274d573815c51029
Ainul Md Razib <ainul.mdrazib@gmail.com>
2023-11-27 20:48:40 +0800

    [Ain] 1 - Implement and style footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="title">
+        track my partnerships
+      </div>
+      <div className="credit">
+        a coding project portfolio by ainul md razib
+      </div>
+
+    </div>
+  );
+}
